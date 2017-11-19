7cc5b239ff46dcb5ba1c544c67f3909b550619ed
warren.dalmeida <warren@local>
2017-11-19 18:33:53 +0100

    Correction du pom et ajout du module front

---
+++ frontweb/src/main/java/eight/ing3/esipe/fr/FrontApplication.java
@@ -0,0 +1,14 @@
+package eight.ing3.esipe.fr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class FrontApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontApplication.class, args);
+	}
+	
+}
