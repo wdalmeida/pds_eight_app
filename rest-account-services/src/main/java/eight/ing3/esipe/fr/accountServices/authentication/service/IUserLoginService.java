a80e756d7e9262c66ada200a084530b8b4ca2ac2
jina.hang <ji-na.hang@etu.u-pec.fr>
2017-12-02 21:21:59 +0100

    Revert "lecture du token dans le header http pour recuperer les claims et les utiliser"
    This reverts commit 147d638f2a7742c902c8185d88cd5cca4b7c3cfd.

--- rest-account-services/src/main/java/eight/ing3/esipe/fr/accountServices/authentication/service/IUserLoginService.java
+++
@@ -1,9 +0,0 @@
-package eight.ing3.esipe.fr.accountServices.authentication.service;
-
-import dto.CredentialDto;
-import dto.UserDto;
-
-public interface IUserLoginService {
-
-    UserDto checkCredentials(CredentialDto credentials);
-}
