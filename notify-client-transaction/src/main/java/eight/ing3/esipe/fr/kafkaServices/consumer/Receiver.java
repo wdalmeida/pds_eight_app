11dfc0a6f00f04d78ecbcf74df771f822fe2c6ce
jina.hang <ji-na.hang@etu.u-pec.fr>
2017-12-05 19:50:10 +0100

    Kafka service with its producer and consumer

--- notify-client-transaction/src/main/java/eight/ing3/esipe/fr/kafkaServices/consumer/Receiver.java
+++ notify-client-transaction/src/main/java/eight/ing3/esipe/fr/kafkaServices/consumer/Receiver.java
@@ -1,18 +1,10 @@
 package eight.ing3.esipe.fr.kafkaServices.consumer;
 
-import java.io.IOException;
-import java.sql.Timestamp;
-import java.util.List;
 import java.util.concurrent.CountDownLatch;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.dataformat.xml.XmlMapper;
-import eight.ing3.esipe.fr.entities.Transaction;
-import eight.ing3.esipe.fr.repositories.NotificationRepository;
-import eight.ing3.esipe.fr.services.NotificationService;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 
 public class Receiver {
@@ -21,34 +13,15 @@ public class Receiver {
 
   private CountDownLatch latch = new CountDownLatch(1);
 
-  @Autowired
-  private XmlMapper xmlMapper;
-
-  @Autowired
-  private NotificationRepository notificationRepository;
-
   public CountDownLatch getLatch() {
     return latch;
   }
 
-
-    NotificationService notificationService = null;
-
-
-    @KafkaListener(topics = "${kafka.topic.transactionQueue}")
-    public void receive(String payload) throws IOException {
+    @KafkaListener(topics = "${kafka.topic.helloworld}")
+    public void receive(String payload) {
+      LOGGER.info("received payload='{}'", payload);
       System.out.println("<< " + payload);
-        List<Transaction> transactions = xmlMapper.readValue(payload, new TypeReference<List<Transaction>>() {
-        });
-        for(Transaction transaction : transactions){
-            System.out.println("j'ai recu une nouvelle transaction !!! ");
-            System.out.println("la transaction est : " + transaction);
-            System.out.println("\n\n");
-//            notificationService.createNotification(transaction.getMontant(), new Timestamp(Long.parseLong(String.valueOf(transaction.getDate()))), transaction.getDetail(), new Integer(String.valueOf(transaction.getMontant())), transaction.getIntitule(), transaction.getIbanrecipient(), transaction.getIntitule(), transaction.getTpe());
-          System.out.println("fin");
-        }
-
-        latch.countDown();
+      latch.countDown();
     }
 
     /*@KafkaListener(topics = "${kafka.topic.boot}")
