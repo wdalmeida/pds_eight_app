d587fb6c50ef9d1eb9dfa2538c2759b62f8a1040
Coline Marin <coline.marin@etu.u-pec.fr>
2017-12-05 19:39:41 +0100

    correction

--- RecupMouvement/src/main/java/repository/ExternMouvInter.java
+++ RecupMouvement/src/main/java/repository/ExternMouvInter.java
@@ -1,10 +1,9 @@
 package repository;
 
-import entities.ExternMouv;
-import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
 public interface ExternMouvInter  {
+
 }
