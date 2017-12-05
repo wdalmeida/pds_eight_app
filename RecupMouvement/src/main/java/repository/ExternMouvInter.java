b01771ed858d0f23270ef4c8200d4dc03afca59e
Coline Marin <coline.marin@etu.u-pec.fr>
2017-12-05 18:57:00 +0100

    api rest recupere fichier xml

---
+++ RecupMouvement/src/main/java/repository/ExternMouvInter.java
@@ -0,0 +1,10 @@
+package repository;
+
+import entities.ExternMouv;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface ExternMouvInter  {
+}
