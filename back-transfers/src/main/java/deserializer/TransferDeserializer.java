c36c9ed1138406b9c6c9a5c394a27683c876c18b
tom.lerouge <tom.g.lerouge@gmail.com>
2018-01-06 15:17:07 +0100

    Addition of kafa consumer

---
+++ back-transfers/src/main/java/deserializer/TransferDeserializer.java
@@ -0,0 +1,37 @@
+package deserializer;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import model.TransferModel;
+import org.apache.kafka.common.serialization.Deserializer;
+
+import java.util.Map;
+
+/**
+ * Created by Tom on 06/01/2018.
+ */
+public class TransferDeserializer implements Deserializer<TransferModel> {
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public void configure(Map<String, ?> arg0, boolean arg1) {
+
+    }
+
+    @Override
+    public TransferModel deserialize(String arg0, byte[] arg1) {
+        ObjectMapper mapper = new ObjectMapper();
+        TransferModel transfer = null;
+        try {
+            transfer = mapper.readValue(arg1, TransferModel.class);
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        }
+        return transfer;
+    }
+
+}
