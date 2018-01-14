91ca24a9b05820e1664ce35b4fe4005deb51ea7d
tom.lerouge <tom.g.lerouge@gmail.com>
2018-01-14 15:26:30 +0100

    Addition of the ability of front transfers component to automatically produce a transfer message from the form

--- front-transfers/src/main/java/producer/TransferProducer.java
+++ front-transfers/src/main/java/producer/TransferProducer.java
@@ -1,10 +1,13 @@
 package producer;
 
+import controller.TransferController;
 import model.TransferModel;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.Properties;
@@ -13,18 +16,38 @@ import static java.time.LocalDate.now;
 
 public class TransferProducer {
 
-    public static void sendTransfer(TransferModel transfer) {
+    private static Logger logger = Logger.getLogger(TransferProducer.class);
+
+    @Value("${consumer.properties.bootstrap_servers_config}")
+    private String bootstrap_servers_config;
+
+    @Value("${consumer.properties.acks_config}")
+    private String acks_config;
+
+    @Value("${consumer.properties.retries_config}")
+    private String retries_config;
+
+    @Value("${consumer.properties.value_serializer_class_config}")
+    private String value_serializer_class_config;
+
+    @Value("${consumer.properties.key_serializer_class_config}")
+    private String key_serializer_class_config;
+
+    @Value("${consumer.properties.topic}")
+    private String topic;
+
+    public void sendTransfer(TransferModel transfer) {
         Properties props = new Properties();
 
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.put(ProducerConfig.ACKS_CONFIG, "all");
-        props.put(ProducerConfig.RETRIES_CONFIG, 0);
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "serializer.TransferSerializer");
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrap_servers_config);
+        props.put(ProducerConfig.ACKS_CONFIG, acks_config);
+        props.put(ProducerConfig.RETRIES_CONFIG, retries_config);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, value_serializer_class_config);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, key_serializer_class_config);
 
         try (Producer<String, TransferModel> producer = new KafkaProducer<>(props)) {
-            producer.send(new ProducerRecord<String, TransferModel>("transfers", transfer));
-            System.out.println("Transfer " + transfer.toString() + " submitted");
+            producer.send(new ProducerRecord<String, TransferModel>(topic, transfer));
+            logger.info("Transfer " + transfer.toString() + " submitted");
             producer.close();
         } catch (Exception e) {
             e.printStackTrace();
@@ -38,7 +61,7 @@ public class TransferProducer {
         test.setBeneficiaryIban("FR768574754");
         test.setSendingIBAN("FR76875686575");
         test.setValueDate(now());
-        TransferProducer.sendTransfer(test);
+        new TransferProducer().sendTransfer(test);
     }
 
 }
