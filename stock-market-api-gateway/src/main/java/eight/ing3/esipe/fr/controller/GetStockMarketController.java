ad8530bcade0ad6a9a96e5d12b278a4a80418551
vyach.sellathurai <vyach.sellathurai@etu.u-pec.fr>
2018-01-18 14:40:58 +0100

    logger init

--- stock-market-api-gateway/src/main/java/eight/ing3/esipe/fr/controller/GetStockMarketController.java
+++ stock-market-api-gateway/src/main/java/eight/ing3/esipe/fr/controller/GetStockMarketController.java
@@ -1,6 +1,8 @@
 package eight.ing3.esipe.fr.controller;
 
 import eight.ing3.esipe.fr.utils.Properties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by Vyach on 18/01/2018.
  */
 @RestController
-public class GetStockMarket {
+public class GetStockMarketController {
+
+    private final Logger logger = LoggerFactory.getLogger(GetStockMarketController.class);
 
     @Autowired
     private Properties properties;
@@ -17,7 +21,7 @@ public class GetStockMarket {
     @RequestMapping("/")
     public String index() {
 
-        System.out.println(this.properties.getStockMarketUrl());
+        logger.info("Mock Stock Market URL : " + properties.getStockMarketUrl());
 
         return "The gateway is running !";
     }
