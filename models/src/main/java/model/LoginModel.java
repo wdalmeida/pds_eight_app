bb3ff4493981c0e812c728f6da315ff26390d7bb
tom.lerouge <tom.g.lerouge@gmail.com>
2018-02-02 15:21:24 +0100

    removal of an useless dependency - not null

--- models/src/main/java/model/LoginModel.java
+++ models/src/main/java/model/LoginModel.java
@@ -1,15 +1,12 @@
 package model;
 
-import com.sun.istack.internal.NotNull;
 import lombok.Data;
 
 @Data
 public class LoginModel {
 
-    @NotNull
     private String login;
 
-    @NotNull
     private String pwd;
 
 
