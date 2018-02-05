ef164c05cf85be73f6bd7613e2391b1bcfc3ac69
jina.hang <ji-na.hang@etu.u-pec.fr>
2018-02-05 21:50:23 +0100

    correction and resolve bugs

--- notify-client-transaction/src/main/java/eight/ing3/esipe/fr/configs/GlobalConfig.java
+++ notify-client-transaction/src/main/java/eight/ing3/esipe/fr/configs/GlobalConfig.java
@@ -1,8 +1,15 @@
 package eight.ing3.esipe.fr.configs;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import eight.ing3.esipe.fr.entities.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @Configuration
 public class GlobalConfig {
@@ -11,5 +18,8 @@ public class GlobalConfig {
     public XmlMapper getXmlMapper() {
         return new XmlMapper();
     }
+
+
+
 
 }
