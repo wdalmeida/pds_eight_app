54793a3ffdb1f9b3094ae39789ffd029c9b27008
vyach.sellathurai <vyach.sellathurai@etu.u-pec.fr>
2018-02-12 03:10:45 +0100

    handling providor response into dto object

--- stock-market-api-gateway/src/main/java/eight/ing3/esipe/fr/provider/IMarketStockProvider.java
+++ stock-market-api-gateway/src/main/java/eight/ing3/esipe/fr/provider/IMarketStockProvider.java
@@ -1,11 +1,12 @@
 package eight.ing3.esipe.fr.provider;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by Vyach on 18/01/2018.
  */
-public interface IMarketStockProvider {
+public interface IMarketStockProvider<T> {
 
     public String getUrlRequest();
     public String getCodeCompany();
@@ -15,7 +16,7 @@ public interface IMarketStockProvider {
     public void setSrcCurrency(String srcCurrency);
     public void setTargetCurrency(String targetCurrency);
 
-    public String handlingResponse(String urlRequest) throws IOException;
+    public List<T> handlingResponse(String urlRequest) throws IOException;
 
     public boolean valideCode(String companyCode);
 }
