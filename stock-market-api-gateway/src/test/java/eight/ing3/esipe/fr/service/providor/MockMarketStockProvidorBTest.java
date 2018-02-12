2ff86810c60e9dc0117a43d1609c58d54e29bf63
vyach.sellathurai <vyach.sellathurai@etu.u-pec.fr>
2018-02-12 17:15:45 +0100

    tu for providor

--- stock-market-api-gateway/src/test/java/eight/ing3/esipe/fr/service/providor/MockMarketStockProvidorBTest.java
+++ stock-market-api-gateway/src/test/java/eight/ing3/esipe/fr/service/providor/MockMarketStockProvidorBTest.java
@@ -1,7 +1,8 @@
 package eight.ing3.esipe.fr.service.providor;
 
-import eight.ing3.esipe.fr.provider.MockMarketStockProviderA;
-import eight.ing3.esipe.fr.provider.dto.DTOProvidorA;
+
+import eight.ing3.esipe.fr.provider.MockMarketStockProviderB;
+import eight.ing3.esipe.fr.provider.dto.DTOProvidorB;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.web.client.ResourceAccessException;
@@ -11,34 +12,32 @@ import java.util.List;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static sun.plugin.javascript.navig.JSType.URL;
 
 /**
  * Created by Vyach on 12/02/2018.
  */
-public class MockMarketStockProvidorATest {
+public class MockMarketStockProvidorBTest {
 
-    private MockMarketStockProviderA providor;
+    private MockMarketStockProviderB providor;
 
-    private static final String URL
-            = "http://int.eight.inside.esiag.info:9090/mock-market-flow-api/marketflow/companies/OR/fromcurrency/USD/tocurrency/EUR";
+    private static final String URL = "http://192.168.4.204:9090/stockmarket-b/stockmarket/companycode/MC";
 
     @Before
     public void init() {
-        providor = new MockMarketStockProviderA();
+        providor = new MockMarketStockProviderB();
     }
 
     @Test
     public void testValideCode() {
 
-        boolean testValide1 = providor.valideCode("OR");
-        boolean testValide2 = providor.valideCode("GLE");
-        boolean testValide3 = providor.valideCode("CA");
+        boolean testValide1 = providor.valideCode("MC");
+        boolean testValide2 = providor.valideCode("KER");
         boolean testNotValid1 = providor.valideCode("");
         boolean testNotValid2 = providor.valideCode("sd");
 
         assertTrue(testValide1);
         assertTrue(testValide2);
-        assertTrue(testValide3);
         assertFalse(testNotValid1);
         assertFalse(testNotValid2);
     }
@@ -50,7 +49,7 @@ public class MockMarketStockProvidorATest {
     @Test
     public void testHandlingResponse() throws IOException {
 
-        List<DTOProvidorA> response = providor.handlingResponse(URL);
+        List<DTOProvidorB> response = providor.handlingResponse(URL);
 
 
         assertTrue(response.size() > 0);
@@ -63,7 +62,7 @@ public class MockMarketStockProvidorATest {
      */
     @Test(expected = ResourceAccessException.class)
     public void testExceptionFromWrongURL() throws IOException {
-        List<DTOProvidorA> responseNull = providor.handlingResponse("http://something.com");
+        List<DTOProvidorB> responseNull = providor.handlingResponse("http://something.com");
 
     }
 }
