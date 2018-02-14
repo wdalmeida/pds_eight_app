fa45e0a31fa98bec6ee4c92918012d0cc6321cbb
vyach.sellathurai <vyach.sellathurai@etu.u-pec.fr>
2018-02-15 00:08:57 +0100

    init project

---
+++ stock-market-client/src/test/java/eight/ing3/esipe/fr/stockmarketclient/StockMarketClientApplicationTests.java
@@ -0,0 +1,16 @@
+package eight.ing3.esipe.fr.stockmarketclient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StockMarketClientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
