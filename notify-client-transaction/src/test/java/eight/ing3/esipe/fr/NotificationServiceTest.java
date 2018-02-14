bcdcf72fffc2b4568e8d314cc838b330f689d153
jina.hang <ji-na.hang@etu.u-pec.fr>
2018-02-14 21:40:04 +0100

    Unit test completed

--- notify-client-transaction/src/test/java/eight/ing3/esipe/fr/NotificationServiceTest.java
+++ notify-client-transaction/src/test/java/eight/ing3/esipe/fr/NotificationServiceTest.java
@@ -3,26 +3,24 @@ package eight.ing3.esipe.fr;
 import eight.ing3.esipe.fr.entities.Transaction;
 import eight.ing3.esipe.fr.services.NotificationService;
 import entity.AccounEntity;
+import entity.NotificationEntity;
+import java.sql.Date;
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.Matchers;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 import repository.AccounRepository;
 import repository.NotificationRepository;
 
-import java.sql.Date;
-import java.sql.Timestamp;
-import java.time.LocalDateTime;
-
-import static java.lang.String.valueOf;
-import static java.time.Instant.now;
-import static org.junit.Assert.assertEquals;
-
 @RunWith(MockitoJUnitRunner.class)
 public class NotificationServiceTest {
 
-
+ 
     @InjectMocks
     NotificationService notificationService;
 
@@ -32,13 +30,17 @@ public class NotificationServiceTest {
     @Mock
     private AccounRepository accounRepository;
 
+    @Before
+    public void before() {
+        this.notificationService.setAccounRepository(accounRepository);
+        this.notificationService.setNotificationRepository(notificationRepository);
+    }
     @Test
     public void shouldCreateNotification(){
         Transaction transaction = new Transaction();
         AccounEntity accounEntity = new AccounEntity();
         accounEntity.setBalance(100);
 
-        //when(accounRepository.findOne(transaction.getIbansender())).thenReturn(accounEntity);
         transaction.setMontant(1000.00);
         transaction.setDate(new Date(01/01/2018));
         transaction.setDetail("détail");
@@ -48,5 +50,8 @@ public class NotificationServiceTest {
 
         boolean created = notificationService.createNotification(transaction);
         assertEquals(true, created);
+        Mockito.verify(this.accounRepository, Mockito.times(1)).findOne(Matchers.any(String.class));
+        Mockito.verify(this.notificationRepository, Mockito.times(1)).save(Matchers.any(NotificationEntity.class));
     }
+    
 }
