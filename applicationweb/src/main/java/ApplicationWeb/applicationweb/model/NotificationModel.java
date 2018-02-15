514bcbdf47c09ecd7330ccaa42e10877de7fbe34
Coline Marin <coline.marin@etu.u-pec.fr>
2018-02-15 18:15:52 +0100

    module complet applicationweb

--- applicationweb/src/main/java/ApplicationWeb/applicationweb/model/NotificationModel.java
+++ applicationweb/src/main/java/ApplicationWeb/applicationweb/model/NotificationModel.java
@@ -1,28 +1,30 @@
+
 package ApplicationWeb.applicationweb.model;
+
 
 
 import javax.persistence.*;
 import java.io.Serializable;
 
-
 @Entity
 @Table(name="notification")
-public class NotificationModel implements Serializable {
+public class NotificationModel{
+
     @Id
-    @SequenceGenerator(name = "seq", sequenceName = "notification_id_notification_seq", allocationSize=0)
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq")
+    //@SequenceGenerator(name = "seq", sequenceName = "notification_id_notification_seq", allocationSize=0)
+    //@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq")
     @Column(name="idnotification")
     private
     Integer idnotification;
 
-    @Column(name="idaccount")
-    private Integer idaccount;
+    @Column(name="iban")
+    private String iban;
 
     @Column(name="label")
     private String label;
 
-    @Column(name="details")
-    private String details;
+    @Column(name="detail")
+    private String detail;
 
     @Column(name="date")
     private String date;
@@ -40,10 +42,10 @@ public class NotificationModel implements Serializable {
     public NotificationModel() {
     }
 
-    public NotificationModel(Integer idaccount, String label, String details, String date, String recipient, Float amount, Boolean status) {
-        this.idaccount = idaccount;
+    public NotificationModel(String iban, String label, String detail, String date, String recipient, Float amount, Boolean status) {
+        this.iban = iban;
         this.label = label;
-        this.details = details;
+        this.detail = detail;
         this.date = date;
         this.recipient = recipient;
         this.amount = amount;
@@ -59,12 +61,12 @@ public class NotificationModel implements Serializable {
         this.idnotification = idnotification;
     }
 
-    public Integer getIdaccount() {
-        return this.idaccount;
+    public String getIban() {
+        return this.iban;
     }
 
-    public void setIdaccount(Integer idaccount) {
-        this.idaccount = idaccount;
+    public void setIban(String iban) {
+        this.iban = iban;
     }
 
     public String getLabel() {
@@ -75,12 +77,12 @@ public class NotificationModel implements Serializable {
         this.label = label;
     }
 
-    public String getDetails() {
-        return this.details;
+    public String getDetail() {
+        return this.detail;
     }
 
-    public void setDetails(String details) {
-        this.details = details;
+    public void setDetail(String detail) {
+        this.detail = detail;
     }
 
     public String getDate() {
@@ -114,4 +116,17 @@ public class NotificationModel implements Serializable {
     public void setStatus(Boolean status) {
         this.status = status;
     }
+
+@Override
+    public String toString(){
+        return "Notification{"+
+                "id="+ idnotification+
+                ", details='"+ amount+ '\''+
+                ", recipient='"+recipient+'\''+
+                ", label='"+label+'\''+
+                ", amount"+amount+
+                '}';
 }
+
+}
+
---
+++ applicationweb/src/main/java/ApplicationWeb/applicationweb/services/Result_DecService.java
@@ -0,0 +1,11 @@
+package ApplicationWeb.applicationweb.services;
+
+import ApplicationWeb.applicationweb.model.NotificationModel;
+import ApplicationWeb.applicationweb.model.Result_DecModel;
+
+import java.util.List;
+
+public interface Result_DecService {
+List<Result_DecModel> findAll();
+
+}
