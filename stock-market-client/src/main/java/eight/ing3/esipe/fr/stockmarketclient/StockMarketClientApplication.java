4610b30d5c3142f7e303799e0faa1f88162f1f06
vyach.sellathurai <vyach.sellathurai@etu.u-pec.fr>
2018-02-15 02:20:46 +0100

    bootstrap added

--- stock-market-client/src/main/java/eight/ing3/esipe/fr/stockmarketclient/StockMarketClientApplication.java
+++ stock-market-client/src/main/java/eight/ing3/esipe/fr/stockmarketclient/StockMarketClientApplication.java
@@ -2,8 +2,10 @@ package eight.ing3.esipe.fr.stockmarketclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class StockMarketClientApplication {
 
 	public static void main(String[] args) {
