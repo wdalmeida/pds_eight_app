12de9991b25f45c4932066ea44cdb686f7c476c2
tom.lerouge <tom.g.lerouge@gmail.com>
2018-03-22 17:20:20 +0100

    Addition of kafka producers for transfer and twitter

---
+++ twitter-producer/src/main/java/producer/TwitterProducer.java
@@ -0,0 +1,40 @@
+package producer;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.Properties;
+
+@Component
+public class TwitterProducer {
+
+    private static Logger logger = Logger.getLogger(TwitterProducer.class);
+
+    private Producer<String, String> producer;
+
+    public void init () {
+        Properties producerProperties = new Properties();
+        producerProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        producerProperties.put(ProducerConfig.ACKS_CONFIG, "all");
+        producerProperties.put(ProducerConfig.RETRIES_CONFIG, 0);
+        producerProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        producerProperties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+
+        producer = new KafkaProducer<>(producerProperties);
+    }
+
+    public void sendTwitterMessage(String message) {
+        producer.send(new ProducerRecord<String, String>("twitter", message));
+        logger.info("twitter message : '" + message + "' is submitted");
+
+    }
+
+    public void stop(){
+        producer.close();
+    }
+}
