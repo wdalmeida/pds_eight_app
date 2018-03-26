62edf6404ac7562977055b3fdcc1da96f6bc0c00
warren.dalmeida <warren@local>
2018-03-26 10:59:05 +0200

    Remove websocket on connection

--- dab_ihm/dab/websocket.py
+++ dab_ihm/dab/websocket.py
@@ -3,11 +3,6 @@ from . import *
 import websocket
 
 
-@socketio.on('client_connected')
-def handle_client_connect_event(json):
-    print('received json: {0}'.format(str(json)))
-
-
 @socketio.on('card_read')
 def handle_nfc_tag_event():
     with app.test_request_context():
