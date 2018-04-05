6b423d2f79f1f32b2e0f149744264589b5fffd7d
warren.dalmeida <warren@local>
2018-04-05 19:49:44 +0200

    Refactoring

--- dab_transaction/rest/controller/controller.py
+++ dab_transaction/rest/controller/controller.py
@@ -1,20 +1,20 @@
-from flask import Flask, render_template,request,redirect,url_for, Response,jsonify,json
+from flask import Flask,request,jsonify,json
 from rest import *
 from rest.service.transaction_service import transaction_service as service
+
 
 @app.route('/transaction/waiting', methods= ['POST'])
 def create_waiting_transaction():
     logging.debug("Controller - Home")
-    resp=None
-    if (request.json and request.headers['Content-Type'] == 'application/json'):
-        data =json.dumps(request.json)
+    if request.json and request.headers['Content-Type'] == 'application/json':
+        data = json.dumps(request.json)
         #add kafka producer
         #add kafka consumer
         resp = jsonify(data)
         resp.status_code = 200
-        resp= service.create_transaction(data,db)
+        resp = service.create_transaction(data,db)
     else:
-        resp= unknown_ressource()
+        resp = unknown_ressource()
     return resp
 
 
