43599d5f5e5b7f0bc920abdf51723988ace75ddd
warren.dalmeida <warren@local>
2018-04-05 13:23:49 +0200

    add some log

--- dab_transaction/message_producer/send_waiting_transaction.py
+++ dab_transaction/message_producer/send_waiting_transaction.py
@@ -9,14 +9,16 @@ def send(transaction):
     producer = KafkaProducer(bootstrap_servers=['10.10.1.110:9092','10.10.1.111:9092'])
     future = producer.send('test', b'raw_bytes')
     # Block for 'synchronous' sends
+    res=False
     try:
         record_metadata = future.get(timeout=10)
         print(record_metadata.topic)
         print(record_metadata.offset)
+        res=True
     except KafkaError:
         print("error")
         pass
     return res
 
 #this is for test
-send("")
+print(send(""))
