99f1e092a0a9b1dc2c8de9318ba206a651c53f91
warren.dalmeida <warren@local>
2018-04-07 12:15:27 +0200

    Add log to kafka procuder

--- dab_transaction/message_producer/send_waiting_transaction.py
+++ dab_transaction/message_producer/send_waiting_transaction.py
@@ -6,7 +6,8 @@ from kafka.errors import KafkaError
 def send(transaction):
     producer = KafkaProducer(bootstrap_servers=['10.10.1.110:9092','10.10.1.111:9092'],
                              value_serializer=lambda m: json.dumps(m).encode('utf-8'))
-    future = producer.send('test', transaction.__dict__)
+    print(transaction)
+    future = producer.send('test', json.dumps(transaction.__dict__))
     #producer = KafkaProducer(bootstrap_servers=['10.10.1.110:9092','10.10.1.111:9092'])
     #future = producer.send('test', b'raw_bytes')
 
