8c760a67796dabf41c1459de4aa5ad9e8e927855
warren.dalmeida <warren@local>
2018-05-14 18:02:46 +0200

    Modification log

--- dab_transaction/rest/service/authentification_service.py
+++ dab_transaction/rest/service/authentification_service.py
@@ -5,14 +5,15 @@ class authentification():
 
     @staticmethod
     def authentification_card(id):
-        logging.debug(type(id))
-        if isinstance(id, str) and len(id) is not 16:
+        logging.debug(isinstance(id, str))
+        logging.debug(len(id))
+        if isinstance(id, str) and len(id) is 16:
             acc = account.Account.get_account_by_card(id)
             if acc is not None:
                 logging.debug(acc.iban)
                 logging.debug(acc.balance)
         else:
-            loggin.debug("Card is not a String or unequal to 27 characters")
+            logging.debug("Card is not a String or unequal to 16 characters")
             acc = None
         return acc
 
