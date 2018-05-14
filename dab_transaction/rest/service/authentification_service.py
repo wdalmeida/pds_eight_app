c650cb1ec5d8574e8bca21ed7fa3c9cc021119da
warren.dalmeida <warren@local>
2018-05-14 17:51:56 +0200

    Controle sur la taille de la carte

--- dab_transaction/rest/service/authentification_service.py
+++ dab_transaction/rest/service/authentification_service.py
@@ -1,13 +1,19 @@
 from rest import *
 
+
 class authentification():
 
     @staticmethod
     def authentification_card(id):
-        acc = account.Account.get_account_by_card(id)
-        if acc is not None:
-            logging.debug(acc.iban)
-            logging.debug(acc.balance)
+        logging.debug(type(id))
+        if isinstance(id, str) and len(id) is not 27:
+            acc = account.Account.get_account_by_card(id)
+            if acc is not None:
+                logging.debug(acc.iban)
+                logging.debug(acc.balance)
+        else:
+            loggin.debug("Card is not a String or unequal to 27 characters")
+            acc = None
         return acc
 
 
