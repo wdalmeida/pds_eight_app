a5b4e876f425db943e738313cd7ccc591c490f69
warren.dalmeida <warren@local>
2018-05-14 17:57:25 +0200

    Correction de la taille de la carte

--- dab_transaction/rest/service/authentification_service.py
+++ dab_transaction/rest/service/authentification_service.py
@@ -6,7 +6,7 @@ class authentification():
     @staticmethod
     def authentification_card(id):
         logging.debug(type(id))
-        if isinstance(id, str) and len(id) is not 27:
+        if isinstance(id, str) and len(id) is not 16:
             acc = account.Account.get_account_by_card(id)
             if acc is not None:
                 logging.debug(acc.iban)
